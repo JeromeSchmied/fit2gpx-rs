593bd6ec97b2460246648d3c0d839ea194adc129
Jeromos Kovacs <iitsnotme214@proton.me>
2024-07-05 14:12:53 +0200

    fix(deps): `fit-rust` -> `fit_file`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use clap::Parser;
-use fit_rust::{
-    protocol::{message_type::MessageType, value::Value, DataMessage, FitMessage},
-    Fit,
-};
 use geo_types::{coord, Point};
 use gpx::{Gpx, GpxVersion, Track, TrackSegment, Waypoint};
-use std::{fs, fs::File, io::BufWriter};
+use std::{fs::File, io::BufWriter};
 use time::OffsetDateTime;
 
 type Res<T> = Result<T, Box<dyn std::error::Error>>;
@@ -16,100 +12,196 @@ struct Args {
     pub files: Vec<String>,
 }
 
-#[derive(Clone, Copy, Default, Debug, PartialEq)]
-struct RecordData {
-    /// latitude
-    pub lat: Option<f64>,
-    /// longitude
-    pub lon: Option<f64>,
-    /// altitude
-    pub alt: Option<f64>,
-    // heart-rate
-    pub hr: Option<u8>,
-    /// timestamp
-    pub time: Option<OffsetDateTime>,
+// FitRecordMsg to gpx Waypoint
+fn frm_to_gwp(frm: FitRecordMsg) -> Waypoint {
+    // "Time: {}\tLat: {}\tLon: {}\tAlt: {:.1}m\tDist: {:.3}km\tSpeed: {:.1}km/h\tHR: {}",
+    // eprintln!("time: {:?}", frm.timestamp);
+    let time = frm.timestamp.unwrap_or(0);
+    let time = OffsetDateTime::from_unix_timestamp(time.into()).ok();
 
-    pub cadence: Option<u8>,
-    pub distance: Option<f64>,
-    pub speed: Option<f64>,
-    pub power: Option<u16>,
-    pub temperature: Option<i8>,
-    pub right_balance: Option<u8>,
-}
-impl RecordData {
-    // crazy check
-    fn invalid(&self) -> bool {
-        self.lat.is_none() && self.lon.is_none()
+    let lat = fit_file::semicircles_to_degrees(frm.position_lat.unwrap_or(0));
+    let lon = fit_file::semicircles_to_degrees(frm.position_long.unwrap_or(0));
+
+    let alt = if let Some(enh_alt) = frm.enhanced_altitude {
+        Some(enh_alt)
+    } else {
+        frm.altitude.map(|alt| alt.into())
     }
+    .map(|alt| alt as f32 / 5. - 500.);
+
+    // let dist = frm.distance.unwrap_or(0) as f32 / 100000.;
+
+    let speed = if let Some(enh_spd) = frm.enhanced_speed {
+        Some(enh_spd)
+    } else {
+        frm.speed.map(|spd| spd.into())
+    }
+    .map(|spd| spd as f64);
+    // .map(|spd| spd as f64 / 1000. * 3.6);
+
+    // let hr = frm
+    //     .heart_rate
+    //     .map(|hr| hr.checked_add(1))
+    //     .unwrap_or(Some(0))
+    //     .unwrap_or(0);
+
+    let geo_point: Point = Point(coord! {x: lon, y: lat});
+
+    let mut wp = Waypoint::new(geo_point);
+
+    wp.elevation = alt.map(|alt| alt.into());
+    wp.time = time.map(|t| t.into());
+    wp.speed = speed;
+
+    wp
 }
-impl From<DataMessage> for RecordData {
-    fn from(value: DataMessage) -> Self {
-        if let MessageType::Record = value.message_type {
-            let lat = value_to_float(df_at(&value, 0));
-            let lon = value_to_float(df_at(&value, 1));
-            let alt = value_to_float(df_at(&value, 2)).map(|alt| alt / 5. - 500.);
 
-            let hr = value_to_float(df_at(&value, 3));
-            let hr: Option<u8> = hr.map(|hr| hr as u8);
+fn no_lat_lon(frm: &FitRecordMsg) -> bool {
+    frm.position_long.is_none() && frm.position_lat.is_none()
+}
 
-            let cadence = value_to_float(df_at(&value, 4));
-            let cadence: Option<u8> = cadence.map(|cad| cad as u8);
+use fit_file::{fit_file, FitRecordMsg, FitSessionMsg};
 
-            let distance = value_to_float(df_at(&value, 5)).map(|d| d / 100000.);
+/// Called for each record message as it is processed.
+fn callback(
+    timestamp: u32,
+    global_message_num: u16,
+    _local_msg_type: u8,
+    _message_index: u16,
+    fields: Vec<fit_file::FitFieldValue>,
+    data: &mut Context,
+) {
+    if global_message_num == fit_file::GLOBAL_MSG_NUM_DEVICE_INFO {
+        // let msg = FitDeviceInfoMsg::new(fields);
+        // println!("{msg:#?}");
+    } else if global_message_num == fit_file::GLOBAL_MSG_NUM_SESSION {
+        let msg = FitSessionMsg::new(fields);
+        let sport_names = fit_file::init_sport_name_map();
+        let sport_id = msg.sport.unwrap();
 
-            let speed = value_to_float(df_at(&value, 6)).map(|v| v / 1000. * 3.6);
+        println!("Sport: {}", sport_names.get(&sport_id).unwrap());
+    } else if global_message_num == fit_file::GLOBAL_MSG_NUM_RECORD {
+        let mut msg = FitRecordMsg::new(fields);
 
-            let power = value_to_float(df_at(&value, 7));
-            let power = power.map(|power| power as u16);
+        data.num_records_processed += 1;
 
-            let temperature = value_to_float(df_at(&value, 13));
-            let temperature = temperature.map(|temperature| temperature as i8);
-
-            let right_balance = value_to_float(df_at(&value, 30));
-            let right_balance = right_balance.map(|right_balance| right_balance as u8);
-
-            let t = df_at(&value, 253);
-            let time = if let Some(Value::Time(t)) = t {
-                if let Ok(t) = OffsetDateTime::from_unix_timestamp((*t).into()) {
-                    Some(t)
-                } else {
-                    None
-                }
-            } else {
-                None
-            };
-
-            RecordData {
-                lat,
-                lon,
-                alt,
-                hr,
-                time,
-                cadence,
-                distance,
-                speed,
-                power,
-                temperature,
-                right_balance,
-            }
+        if let Some(ts) = msg.timestamp {
+            assert_eq!(timestamp, ts);
         } else {
-            RecordData::default()
+            msg.timestamp = Some(timestamp);
         }
+
+        if no_lat_lon(&msg) {
+            data.no_lat_lon_sum += 1;
+        }
+
+        // println!(
+        //     "timestamp: {:?}|{:?}|{:?}|{}",
+        //     msg.timestamp, msg.time128, msg.time_from_course, timestamp
+        // );
+        let wp = frm_to_gwp(msg);
+        data.track_segment.points.push(wp);
+        // assert!(msg.timestamp.is_some());
+
+        // println!("{msg:#?}");
+
+        // println!(
+        //     // "Timestamp: {} Latitude: {} Longitude: {} Altitude: {} Distance: {} Speed: {} HeartRate: {}",
+        //     "Time: {}\tLat: {}\tLon: {}\tAlt: {:.1}m\tDist: {:.3}km\tSpeed: {:.1}km/h\tHR: {}",
+        //     msg.timestamp.unwrap_or(0),
+        //     fit_file::semicircles_to_degrees(msg.position_lat.unwrap_or(0)),
+        //     fit_file::semicircles_to_degrees(msg.position_long.unwrap_or(0)),
+        //     msg.enhanced_altitude.unwrap_or(0) as f32 / 5. - 500.,
+        //     msg.distance.unwrap_or(0) as f32 / 100000.,
+        //     msg.enhanced_speed.unwrap_or(0) as f32 / 1000. * 3.6,
+        //     msg.heart_rate
+        //         .map(|hr| hr.checked_add(1))
+        //         .unwrap_or(Some(0))
+        //         .unwrap_or(0),
+        // );
     }
 }
-impl From<RecordData> for Waypoint {
-    fn from(value: RecordData) -> Self {
-        let geo_point: Point =
-            Point(coord! {x: value.lon.unwrap_or(0.), y: value.lat.unwrap_or(0.)});
 
-        let mut wp = Waypoint::new(geo_point);
-        wp.elevation = value.alt;
-        wp.time = value.time.map(|t| t.into());
-        wp.speed = value.speed;
-
-        wp
-    }
+/// Context structure. An instance of this will be passed to the parser and ultimately to the callback function so we can use it for whatever.
+#[derive(Default)]
+struct Context {
+    no_lat_lon_sum: u32,
+    num_records_processed: u16,
+    track_segment: TrackSegment,
 }
+
+fn fit2gpx(f_in: &str) -> Res<()> {
+    let file = std::fs::File::open(f_in)?;
+
+    let mut reader = std::io::BufReader::new(file);
+    let mut cx = Context::default();
+    fit_file::read(&mut reader, callback, &mut cx)?;
+
+    let percent_no_lat_lon = cx.no_lat_lon_sum as f32 / cx.track_segment.points.len() as f32;
+    if cx.no_lat_lon_sum > 0 && percent_no_lat_lon < 0.9 {
+        eprintln!("less than 90% ({} out of {} = {}) doesn't contain latitude and longitude => deleting these points", cx.no_lat_lon_sum, cx.track_segment.points.len(), percent_no_lat_lon);
+        cx.track_segment
+            .points
+            .retain(|point| point.point().x_y() != (0., 0.));
+    }
+
+    // Instantiate Gpx struct
+    let track = Track {
+        name: None,
+        comment: None,
+        description: None,
+        source: None,
+        links: vec![],
+        type_: None,
+        number: None,
+        segments: vec![cx.track_segment],
+    };
+    let gpx = Gpx {
+        version: GpxVersion::Gpx11,
+        creator: None,
+        metadata: None,
+        waypoints: vec![],
+        tracks: vec![track],
+        routes: vec![],
+    };
+
+    let f_out = f_in.replace(".fit", ".gpx");
+    // Create file at path
+    let gpx_file = File::create(f_out)?;
+    let buf = BufWriter::new(gpx_file);
+
+    // Write to file
+    gpx::write(&gpx, buf)?;
+    println!("{} records processed", cx.num_records_processed);
+    Ok(())
+}
+
+//                 if let MessageType::Record = msg.data.message_type {
+//                     let rec_dat: RecordData = msg.data.clone().into();
+//                     if rec_dat.no_lat_lon() {
+//                         no_lat_lon_sum += 1;
+//                         // eprintln!("doesn't contain lat/lon data: {:?}", msg.data);
+//                         // continue;
+//                     }
+//                     // eprintln!("{rec_dat:#?}");
+//                     // Add track point
+//                     let wp: Waypoint = rec_dat.into();
+//                     // if wp.point().x_y() == (0., 0.) {
+//                     //     eprintln!("warn: guess it's invalid: {msg:#?}");
+//                     // }
+//                     if ongoing_activity {
+//                         track_segment.points.push(wp);
+//                     } else {
+//                         eprintln!("warn: NOT in an activity right now");
+//                         // std::io::stdin().read_line(&mut String::new())?;
+//                     }
+//                 } else if let MessageType::Activity = msg.data.message_type {
+//                     let start_stop = df_at(&msg.data, 4);
+//                     if let Some(Value::Enum(start_stop)) = start_stop {
+//                         if start_stop == &"start" {
+//                             ongoing_activity = true;
+//                         } else if start_stop == &"stop" {
+//                             ongoing_activity = false;
 
 fn main() {
     // collecting cli args
@@ -134,127 +226,4 @@ fn main() {
             continue;
         }
     }
-}
-
-fn fit2gpx(f_in: &String) -> Res<()> {
-    let file = fs::read(f_in)?;
-    let fit: Fit = Fit::read(file)?;
-    eprintln!("file: {f_in}");
-    // let mut log_file = File::create([f_in, ".log"].concat())?;
-
-    // println!("\n\nHEADER:");
-    // println!("\theader size: {}", &fit.header.header_size);
-    // println!("\tprotocol version: {}", &fit.header.protocol_version);
-    // println!("\tprofile version: {}", &fit.header.profile_version);
-    // println!("\tdata_size: {}", &fit.header.data_size);
-    // println!("\tdata_type: {}", &fit.header.data_type);
-    // println!("\tcrc: {:?}", &fit.header.crc);
-    // println!("-----------------------------\n");
-
-    let mut track_segment = TrackSegment { points: vec![] };
-    let mut ongoing_activity = true;
-
-    for data in &fit.data {
-        match data {
-            FitMessage::Definition(_msg) => {
-                // println!("\nDefinition: {:#?}", msg.data);
-            }
-            FitMessage::Data(msg) => {
-                // writeln!(log_file, "\nData: {msg:#?}")?;
-                // println!("\nData: {:#?}", msg);
-                if let MessageType::Record = msg.data.message_type {
-                    let rec_dat: RecordData = msg.data.clone().into();
-                    if rec_dat.invalid() {
-                        eprintln!("doesn't contain lat/lon data: {:?}", msg.data);
-                        continue;
-                    }
-                    // eprintln!("{rec_dat:#?}");
-
-                    // Add track point
-                    let wp: Waypoint = rec_dat.into();
-                    // if wp.point().x_y() == (0., 0.) {
-                    //     eprintln!("warn: guess it's invalid: {msg:#?}");
-                    // }
-                    if ongoing_activity {
-                        track_segment.points.push(wp);
-                    } else {
-                        eprintln!("warn: NOT in an activity right now");
-                        // std::io::stdin().read_line(&mut String::new())?;
-                    }
-                } else if let MessageType::Activity = msg.data.message_type {
-                    let start_stop = df_at(&msg.data, 4);
-                    if let Some(Value::Enum(start_stop)) = start_stop {
-                        if start_stop == &"start" {
-                            ongoing_activity = true;
-                        } else if start_stop == &"stop" {
-                            ongoing_activity = false;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // Instantiate Gpx struct
-    let track = Track {
-        name: None,
-        comment: None,
-        description: None,
-        source: None,
-        links: vec![],
-        type_: None,
-        number: None,
-        segments: vec![track_segment],
-    };
-    let gpx = Gpx {
-        version: GpxVersion::Gpx11,
-        creator: None,
-        metadata: None,
-        waypoints: vec![],
-        tracks: vec![track],
-        routes: vec![],
-    };
-
-    let f_out = f_in.replace(".fit", ".gpx");
-    // Create file at path
-    let gpx_file = File::create(f_out)?;
-    let buf = BufWriter::new(gpx_file);
-
-    // Write to file
-    gpx::write(&gpx, buf)?;
-
-    Ok(())
-}
-
-fn value_to_float(val: Option<&Value>) -> Option<f64> {
-    match val {
-        Some(Value::U8(x)) => Some(*x as f64),
-        Some(Value::U16(x)) => Some(*x as f64),
-        Some(Value::U32(x)) => Some(*x as f64),
-        Some(Value::U64(x)) => Some(*x as f64),
-
-        Some(Value::I8(x)) => Some(*x as f64),
-        Some(Value::I16(x)) => Some(*x as f64),
-        Some(Value::I32(x)) => Some(*x as f64),
-        Some(Value::I64(x)) => Some(*x as f64),
-
-        Some(Value::F32(x)) => Some(*x as f64),
-        Some(Value::F64(x)) => Some(*x),
-        _x => {
-            // eprintln!("invalid f64: {x:?}");
-            None
-        }
-    }
-}
-
-/// datafield at num
-fn df_at(data_msg: &DataMessage, num: u8) -> Option<&Value> {
-    // eprintln!("data-msg: {data_msg:#?}");
-    let x = data_msg
-        .values
-        .iter()
-        .filter(|df| df.field_num == num)
-        .collect::<Vec<_>>();
-
-    Some(&x.first()?.value)
 }
